6f036733323f1c206ed5dd863de24859bd8eafca
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-10-27 10:52:30 +0800

    Update configuration.nix

--- examples/hkdm/configuration.nix
+++ examples/hkdm/configuration.nix
@@ -1,28 +1,50 @@
 { config, lib, pkgs, ... }:
 
 let
+  inherit (lib) mkForce;
+  system_type = config.mobile.system.type;
+
   defaultUserName = "alice";
 in
 {
   imports = [
     ./hkdm.nix
     #./bluetooth.nix
-    #../common-configuration.nix
+    ../common-configuration.nix
   ];
 
-  config = {
-    users.users."${defaultUserName}" = {
-      isNormalUser = true;
-      password = "1234";
-      extraGroups = [
+  config = lib.mkMerge [
+    # INSECURE STUFF FIRST
+    # Users and hardcoded passwords.
+    {
+      # Forcibly set a password on users...
+      # Note that a numeric password is currently required to unlock a session
+      # with the plasma mobile shell :/
+     
+      
+      users.users.${defaultUserName} = {
+        isNormalUser = true;
+        # Numeric pin makes it **possible** to input on the lockscreen.
+        password = "1234";
+        home = "/home/${defaultUserName}";
+        extraGroups = [
+      #    "dialout"
+      #    "feedbackd"
+          "networkmanager"
+          "video"
+          "wheel"
+        ];
+        uid = 1000;
+      };
 
-      ];
-    };
-  users.users.root.password  = "nixos";
-  # mobile.quirks.qualcomm.sdm845-modem.enable = true;
-  nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "24.11";  
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+      users.users.root.password  = "nixos";
+      # mobile.quirks.qualcomm.sdm845-modem.enable = true;
+      nixpkgs.config.allowUnfree = true;
+      system.stateVersion = "24.11";
+      nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+    }
+
     # Networking, modem and misc.
     {
       # Ensures any rndis config from stage-1 is not clobbered by NetworkManager
@@ -38,5 +60,5 @@ in
       # Not a good idea given the fact this config is insecure (well-known password).
       services.openssh.enable = true;
     }
-  };
+  ];
 }
