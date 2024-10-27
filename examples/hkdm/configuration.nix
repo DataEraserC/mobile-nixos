217d04cf90b3c775cbb598edddb125e09de97758
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-10-27 10:46:53 +0800

    Update configuration.nix

--- examples/hkdm/configuration.nix
+++ examples/hkdm/configuration.nix
@@ -18,7 +18,25 @@ in
 
       ];
     };
-    
+  users.users.root.password  = "nixos";
+  # mobile.quirks.qualcomm.sdm845-modem.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  system.stateVersion = "24.11";  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    # Networking, modem and misc.
+    {
+      # Ensures any rndis config from stage-1 is not clobbered by NetworkManager
+      networking.networkmanager.unmanaged = [ "rndis0" "usb0" ];
 
+      # Setup USB gadget networking in initrd...
+      mobile.boot.stage-1.networking.enable = lib.mkDefault true;
+    }
+
+    # SSH
+    {
+      # Start SSH by default...
+      # Not a good idea given the fact this config is insecure (well-known password).
+      services.openssh.enable = true;
+    }
   };
 }
